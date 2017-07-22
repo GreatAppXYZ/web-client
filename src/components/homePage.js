87e89ff75e715bd5f6d19da89534df0d892ec0f0
alayor <alayor3@gmail.com>
2017-07-22 14:54:05 -0500

    Adding transition rules.

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass({
    render: function() {
@@ -8,6 +10,7 @@ var Home = React.createClass({
            <div className="jumbotron">
                <h1>Great App XYZ</h1>
                <p>This is a really Great App!</p>
+               <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
            </div>
        );
    }
