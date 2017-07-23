c6a953f76df5bb496c463b7268255d4ac2d2a9de
alayor <alayor3@gmail.com>
2017-07-23 14:26:52 -0500

    Creating first React Components and React Router Configuration.

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application uses React and consumes Spring Boot REST Api</p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,17 @@
+import React, {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        Courses
+      </div>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+};
+
+
+export default CoursesPage;
