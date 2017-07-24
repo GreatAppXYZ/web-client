8360343d39b426b62fc5d197c141cbec032cee68
alayor <alayor3@gmail.com>
2017-07-23 19:01:07 -0500

    Adding support to get information asynchronously from Api.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,48 +2,26 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
    super(props, context);
-
-   this.state = {
-     course: {title: ""}
-   };
-
-   this.onTitleChange = this.onTitleChange.bind(this);
-   this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
-  }
-
-  courseRow(course, index) {
-    return <div key={index}>{course.title}</div>;
   }
 
   render() {
+    const {courses} = this.props;
+    debugger;
+    if(courses.length === 0) {
+      return <div>
+        <h1>Courses</h1>
+        <p>Loading...</p>
+      </div>
+    }
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
+        <CourseList courses={courses} />
       </div>
     );
   }
