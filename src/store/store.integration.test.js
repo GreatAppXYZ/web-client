f30c29052ef98ac46cbbc9789cc6027a9210e44a
alayor <alayor3@gmail.com>
2017-07-24 18:23:08 -0500

    Creating integration tests for Redux reducers.

---
+++ src/store/store.integration.test.js
@@ -0,0 +1,26 @@
+import expect from 'expect';
+import {createStore} from 'redux';
+import rootReducer from '../reducers';
+import initialState from '../reducers/initialState';
+import * as courseActions from '../actions/courseActions';
+
+describe('Store', function () {
+ it('Should handle creating courses', function() {
+   //given
+   const store = createStore(rootReducer, initialState);
+   const course = {
+     title: "Clean Code"
+   };
+
+   //when
+   const action = courseActions.createCourseSuccess(course);
+   store.dispatch(action);
+
+   //then
+   const actual = store.getState().courses[0];
+   const expected = {
+     title: 'Clean Code'
+   };
+   expect(expected, actual);
+ })
+});
